6daee77fdbdc0b5742cf8ae6e12d183fa1de47b3
Carlos Almeida <carlosmx7@gmail.com>
2022-08-28 16:30:00 -0300

    Adicionando OlaMundo

---
+++ AprendendoCSharp/1-OlaMundo/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ola, Mundo!");
+        Console.WriteLine("Tecle Enter para fechar...");
+        Console.ReadLine();
+    }
+}
