a66e652654c65b5424aec2111549ab4f92dc532b
CARLOS ALBERTO TEIXEIRA ALMEIDA <carlos.teixeira@csn.com.br>
2022-08-29 14:44:45 -0300

    Adicionando aulas 3 e 4 Curso CSharp

---
+++ AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Progama
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variaveis Ponto Flutuante");
+
+        double salario;
+        salario = 3000.13;
+
+        double idade;
+        idade = 7.0 / 5;
+
+
+        Console.WriteLine(salario);
+        Console.WriteLine(idade);
+
+        Console.WriteLine("Tecle Enter para fecha...");
+        Console.ReadLine();
+
+    }
+}
---
+++ AprendendoCSharp/4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+    class Programa
+{
+    static void Main(string[] args)
+    {
+        
+        double salario;
+        salario = 3000.15;
+        Console.WriteLine(salario);
+        
+        int salarioInteiro;
+        salarioInteiro = (int)salario;
+        Console.WriteLine(salarioInteiro);
+       
+        // O long é um tipo de varial de 64 bits
+        long x;
+        x = 2000000000000000;
+        Console.WriteLine(x);
+        
+        short y;
+        y = 15000;
+        Console.WriteLine(y);
+       
+        float altura;
+        altura = 1.62f;
+        Console.WriteLine(altura);
+
+        // ou conforme abaixo para qualquer variavel:
+                
+        float altura2 = 1.62f;
+        Console.WriteLine(altura2);
+
+        Console.WriteLine("Tecle Enter para fechar ...");
+        Console.ReadLine();
+
+    }
+}
