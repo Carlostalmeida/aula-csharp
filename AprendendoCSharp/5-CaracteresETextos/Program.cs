78269a76ed346b202c649e6ac601650d99bf2e31
Carlos Almeida <carlosmx7@gmail.com>
2022-09-04 22:30:52 -0300

    Adicionando aulas 5 e 6 de Csharp

---
+++ AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 5 - Carcteres e Textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        letra = (char)(86 + 1);
+        Console.WriteLine(letra);
+
+        string primeiraFrase = "Alura - Curso de Tecnologia ";
+        Console.WriteLine(primeiraFrase + 2022);
+
+        string vazia = "";
+        Console.Write(vazia);
+
+        string cursos = @"Cursos Disponiveis: 
+-GO 
+-C# 
+-Python 
+-Java";
+        Console.WriteLine(cursos);
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
---
+++ AprendendoCSharp/6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando o projeto 6 - Atribuicoes de Variaveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
