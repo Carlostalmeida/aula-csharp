80d55bb501153321c5d14f209a200c69b6d13d45
CARLOS ALBERTO TEIXEIRA ALMEIDA <carlos.teixeira@csn.com.br>
2022-09-27 07:46:22 -0300

    Adicionando atualização da classe cliente e novas funções

--- Curso/bytebank/bytebank/ContaCorrente.cs
+++ Curso/bytebank/bytebank/ContaCorrente.cs
@@ -1,12 +1,15 @@
-﻿namespace bytebank 
+﻿using bytebank.Titular;
+using System.Net.NetworkInformation;
+
+namespace bytebank 
 {
     public class ContaCorrente
     {
-        public string titular;
+        public Cliente titular;
         public string conta;
         public int numero_agencia;
         public string nome_agencia;
-        public double saldo;
+        private double saldo;
         public bool verificador;
 
         public bool Sacar(double valor)
@@ -30,5 +33,42 @@
         {
             saldo = saldo + valor;
         }
+
+        public bool Transferir(double valor, ContaCorrente destino)
+        {
+            if (saldo < valor)
+            {
+                return false;
+            }
+            if(valor < 0)
+            {
+                return false;
+            }
+            else
+            {
+                saldo = saldo - valor;
+                destino.saldo = destino.saldo + valor;
+                return true;
+            }
+        }
+
+        public void DefinirSaldo(double valor)
+        {
+            if(valor < 0)
+            {
+                return;
+            }
+            saldo = valor;
+        }
+
+        public void ExibirDadosDaConta()
+        {
+            Console.WriteLine("Titular: " + titular);
+            Console.WriteLine("Conta: " + conta);
+            Console.WriteLine("Numero Agencia: " + numero_agencia);
+            Console.WriteLine("Nome Agencia: " + nome_agencia);
+            Console.WriteLine("Saldo: " + saldo);
+        }
+
     }
 }
--- Curso/bytebank/bytebank/Program.cs
+++ Curso/bytebank/bytebank/Program.cs
@@ -1,21 +1,39 @@
 ﻿using bytebank;
+using bytebank.Titular;
 
 Console.WriteLine("Boas Vindas ao seu Banco, ByteBank!");
 
 
-ContaCorrente conta1 = new ContaCorrente();
-conta1.titular = "Andre Silva";
-conta1.conta = "10123 - x";
-conta1.numero_agencia = 23;
-conta1.nome_agencia = "Agencia Central";
-conta1.saldo = 100;
+//ContaCorrente conta1 = new ContaCorrente();
+//conta1.titular = "Andre Silva";
+//conta1.conta = "10123 - x";
+//conta1.numero_agencia = 23;
+//conta1.nome_agencia = "Agencia Central";
+//conta1.saldo = 100;
 
-ContaCorrente conta2 = new ContaCorrente();
-conta2.titular = "Amanda Silva";
-conta2.conta = "111999 - x";
-conta2.numero_agencia = 58;
-conta2.nome_agencia = "Agencia Central";
-conta2.saldo = 100;
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titular = "Amanda Silva";
+//conta2.conta = "111999 - x";
+//conta2.numero_agencia = 58;
+//conta2.nome_agencia = "Agencia Central";
+//conta2.saldo = 100;
+
+//Cliente cliente = new Cliente();
+//cliente.nome = "Andre Silva";
+//cliente.cpf = "10253541263";
+//cliente.profissao = "Programador C#";
+
+ContaCorrente conta3 = new ContaCorrente();
+conta3.titular = new Cliente();
+conta3.titular.nome = "Andre Silva";
+conta3.titular.cpf = "10253541263";
+conta3.titular.profissao = "Programador C#";
+conta3.conta = "2513252 - x";
+conta3.numero_agencia = 35;
+conta3.nome_agencia = "Agencia Central";
+conta3.saldo = 100;
+
+Console.WriteLine(conta3.titular.nome);
 
 
 //Console.WriteLine("Titular: " + conta1.titular);
@@ -24,23 +42,18 @@ conta2.saldo = 100;
 //Console.WriteLine("Nome Agencia: " + conta1.nome_agencia);
 //Console.WriteLine("saldo: " + conta1.saldo);
 
-Console.WriteLine("Saldo da Amanda pre-saque: " +conta2.saldo);
+//Console.WriteLine(cliente.nome);
 
-bool saque = conta2.Sacar(50);
+//Console.WriteLine("Saldo da Amanda pre-Transferencia: " +conta2.saldo);
 
-Console.WriteLine("Saque realizardo? "+saque);
+//Console.WriteLine("Saldo da Andre pre-Transferenciaa: " + conta1.saldo);
 
-Console.WriteLine("Saldo da Amanda pos-saque: " + conta2.saldo);
+//bool tranferencia = conta1.Transferir(50, conta2);
 
-conta2.Depositar(60);
+//Console.WriteLine("Transferencia Realizada com sucesso? " +tranferencia);
 
-Console.WriteLine("Saldo da Amanda pos-deposito: " + conta2.saldo);
+//Console.WriteLine("Saldo da Amanda pos-Transferencia: " + conta2.saldo);
 
-//Console.WriteLine("Titular: " + conta3.titular);
-//Console.WriteLine("Conta: " + conta3.conta);
-//Console.WriteLine("Numero Agencia: " + conta3.numero_agencia);
-//Console.WriteLine("Nome Agencia: " + conta3.nome_agencia);
-//Console.WriteLine("saldo: " + conta3.saldo);
-//Console.WriteLine("Verificador: " + conta3.verificador);
+//Console.WriteLine("Saldo da Andre pos-Tranferencia: " + conta1.saldo);
 
 Console.ReadKey();
